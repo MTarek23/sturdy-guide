df53342aeb4f2406cb85eb5430f95d5707943fe4
mtelewa <mohamed.hassan@kit.edu>
2022-09-28 13:21:55 +0200

    testing

--- lib/dtool_dataset.py
+++ lib/dtool_dataset.py
@@ -1,20 +1,26 @@
 import os
-import dtoolcore
 from ruamel.yaml import YAML
+import dtoolcore
+from dtool_create.dataset import _get_readme_template
 
-
-def create_dataset(dataset_name, **kwargs):
+def create_dataset(dataset_name):
     """
     Creates a parent dataset
     """
-    
+
     # global yaml setting
     yaml = YAML()
     yaml.explicit_start = True
     yaml.indent(mapping=4, sequence=4, offset=2)
 
     ds = dtoolcore.DataSetCreator(dataset_name, os.getcwd(), creator_username='mtelewa')
-    metadata = manipulate_ds(dataset_name).update_readme(**kwargs)
+
+    read_from = os.path.join(os.path.expanduser('~'), '.dtool', 'custom_dtool_readme.yml')
+    readme_template = _get_readme_template(read_from)
+    # load readme template and update
+    metadata = yaml.load(readme_template)
+
+    # metadata = update_readme(**kwargs)
     sim_uri = os.path.join(os.getcwd(),dataset_name)
     template = os.path.join(sim_uri, 'README.yml')
 
