fae8487b07aa802b7d41c6e72372bd131b88f9c5
mtelewa <mohamed.hassan@kit.edu>
2022-10-02 18:08:45 +0200

    testing

--- lib/post_commands.py
+++ lib/post_commands.py
@@ -7,10 +7,10 @@ def grid(ds, Nchunks, slice_size, fluid, stable_start, stable_end, pump_start, p
         1 {Nchunks} {slice_size} {fluid} {stable_start} {stable_end} {pump_start} {pump_end}"
 
 def merge(ds, Nchunks):
-    return f"if [ ! -f {ds}_{Nchunks}x1_001.nc ]; then \n
-      mv {ds}_{Nchunks}x1_000.nc {ds}_{Nchunks}x1.nc ; \n
-      mv {ds}_1x{Nchunks}_000.nc {ds}_1x{Nchunks}.nc ; \n
-    else \n
+    return f"if [ ! -f {ds}_{Nchunks}x1_001.nc ]; then \
+      mv {ds}_{Nchunks}x1_000.nc {ds}_{Nchunks}x1.nc ; \
+      mv {ds}_1x{Nchunks}_000.nc {ds}_1x{Nchunks}.nc ; \
+    else \
       cdo mergetime {ds}_{Nchunks}x1_*.nc {ds}_{Nchunks}x1.nc ; rm {ds}_{Nchunks}x1_* ;\
       cdo mergetime {ds}_1x{Nchunks}_*.nc {ds}_1x{Nchunks}.nc ; rm {ds}_1x{Nchunks}_* ;\
     fi"
